215f3cb8b5cd4486cf9c05a7cfe2f55b89bbe961
yichang.yyf <yangyifei.yyf@antgroup.com>
2023-02-06 11:31:11 +0800

    Add argument for memory limit and cpu count. Update the default value of src_dir and deploy_ip_list

--- compile_and_deploy_cluster.py
+++ compile_and_deploy_cluster.py
@@ -1,30 +1,42 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from typing import *
 import os
 import argparse
 import subprocess
 import re
+import socket
+
+work_dir = os.path.abspath(os.curdir)
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+username = os.getlogin()
 
 parser = argparse.ArgumentParser(description='compile and deploy cluster from source code directory')
-parser.add_argument('--src_dir', type=str, required=True,
-                    help='OB源代码绝对路径，必填')
-parser.add_argument('--deploy_dir', type=str, required=True,
-                    help='部署目录名称，绝对路径，若目录不存在则新建，必填')
-parser.add_argument('--deploy_ip_list', type=str, nargs='+', required=False,
-                    help='要部署的IP列表，默认为本机两副本部署，如果在同一IP上部署多个Observer需要把IP重写多次')
+parser.add_argument('--src_dir', type=str, default=work_dir,
+                    help='OB源代码绝对路径。若为空则默认为当前执行路径')
+parser.add_argument('--deploy_ip_list', type=str, nargs='+', default=[ip, ip],
+                    help='要部署的IP列表，如果在同一IP上部署多个observer需要把IP重写多次。若为空则默认本机两副本部署')
+parser.add_argument('--deploy_dir', type=str, default='/data/1/'+username,
+                    help='部署目录名称，绝对路径，若目录不存在则新建。若为空则默认为/data/1/$USER')
 parser.add_argument('--compile', default=None, choices=['debug', 'release'], type=str,
                     help='本次部署是否需要从源代码编译出一个新的binary，若需要重编译请填写编译模式')
-parser.add_argument('--tag', type=str, default='test', required=False,
-                    help='生成的OBD镜像名称以及部署的集群名称，默认为test')
+parser.add_argument('--tag', type=str, default='test',
+                    help='生成的OBD镜像名称以及部署的集群名称。若为空则默认为test')
 parser.add_argument('--with_admin',  default=False, type=bool,
-                    help='编译并复制ob_admin，默认不编译')
+                    help='编译ob_admin'),
+parser.add_argument('--enable_oracle', default=False, action='store_true',
+                    help='是否生成Oracle租户'),
+parser.add_argument('--memory_limit', type=str, default='8G',
+                   help='单个observer的内存限制。若为空则默认为8G'),
+parser.add_argument('--cpu_count', type=str, default='4',
+                   help='单个observer的CPU限制。若为空则默认为4'),
+
+
 args = parser.parse_args()
 
 server_config = ''
-
-self_ip = os.popen('hostname -i').read().strip()
-print('self_ip:{}'.format(self_ip))
-if args.deploy_ip_list is None:
-  args.deploy_ip_list = [self_ip, self_ip]# 如果不填写部署ip，那么就在本机两副本部署
 for idx in range(len(args.deploy_ip_list)):
   server_config += '\n    - name: server{}\n      ip: {}'.format(idx + 1, args.deploy_ip_list[idx])
 ip_used_port = {}
@@ -54,9 +66,9 @@ config_yaml = '''oceanbase-ce:
   global:
     devname: {}
     cluster_id: 1
-    memory_limit: 8G # The maximum running memory for an observer
+    memory_limit: {} # The maximum running memory for an observer
     system_memory: 3G # The reserved system memory. system_memory is reserved for general tenants.
-    cpu_count: 4 # The assigned cpu for an observer
+    cpu_count: {} # The assigned cpu for an observer
     datafile_size: 10G # The disk space for data storage.
     log_disk_size: 10G # The disk space for redo log storage.
     syslog_level: TRACE # System log level. The default value is INFO.
@@ -64,7 +76,7 @@ config_yaml = '''oceanbase-ce:
     enable_syslog_wf: false # Print system logs whose levels are higher than WARNING to a separate log file. The default value is true.
     enable_syslog_recycle: true # Enable auto system log recycling or not. The default value is false.
     max_syslog_file_count: 100 # The maximum number of reserved log files before enabling auto recycling. The default value is 0.
-    __min_full_resource_pool_memory: 1073741824 # Lower bound of memory for resource pool, default 5G{}'''.format(args.tag, server_config, devname, server_config_detail)
+    __min_full_resource_pool_memory: 1073741824 # Lower bound of memory for resource pool, default 5G{}'''.format(args.tag, server_config, devname, args.memory_limit, args.cpu_count, server_config_detail)
 with open('config.yaml', 'w') as f:
   f.write(config_yaml)
 
@@ -73,7 +85,6 @@ def assert_notice(cmd : str, info : str = None):
   assert(0 == os.system(cmd))
   if str:
     print('[NOTICE]{}'.format(info))
-
 
 work_dir = os.path.abspath(os.curdir)
 os.chdir(args.src_dir)
@@ -107,4 +118,5 @@ if args.compile and args.with_admin:
     assert_notice('scp {}/build_{}/tools/ob_admin/ob_admin {}:{}'.format(args.src_dir, args.compile, ip, args.deploy_dir), 'copy ob_admin to deloy dir done')
 assert_notice('obd cluster start {}'.format(args.tag), 'cluster start done, cluster:{}'.format(args.tag))
 #assert_notice('obd cluster tenant create {} -n mysql --max-cpu 2 --mode mysql'.format(args.tag), 'cluster create mysql tenant done, cluster:{}, tenant:mysql'.format(args.tag))
-#assert_notice('obd cluster tenant create {} -n oracle --max-cpu 2 --mode oracle'.format(args.tag), 'cluster create oracle tenant done, cluster:{}, tenant:oracle'.format(args.tag))
+if args.enable_oracle:
+	assert_notice('obd cluster tenant create {} -n oracle --max-cpu 2 --mode oracle'.format(args.tag), 'cluster create oracle tenant done, cluster:{}, tenant:oracle'.format(args.tag))
